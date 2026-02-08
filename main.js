8baea414009650a5a7778ea7fe3c464933b86ed9
JoeDev37 <alonejoe367@gmail.com>
2026-02-08 19:32:16 +0300

    threeNumGame coding

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 
 console.log('Hello, World');
+
+const numOne = document.getElementById('numOne');
+const numTwo = document.getElementById('numTwo');
+const numThree = document.getElementById('numThree');
+
+const round = document.getElementById('round');
+
+const restartBtn = document.getElementById('restart');
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    const numElements = document.querySelectorAll('.num');
+
+    setInterval(() => {
+        numElements.forEach(num => {
+        num.textContent = Math.floor(Math.random() * 10);
+    });
+    }, 700);
+})
