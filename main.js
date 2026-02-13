c058020bc08611263d2ac0937a37df13b4e0400d
JoeDev37 <alonejoe367@gmail.com>
2026-02-13 20:06:26 +0300

    threeNumGame coding

--- main.js
+++ main.js
@@ -9,30 +9,85 @@ const round = document.getElementById('round');
 
 const restartBtn = document.getElementById('restart');
 
+let gameInterval = null;
+
+let spin = {
+    numOne: null,
+    numTwo: null,
+    numThree: null
+}
 
 document.addEventListener('DOMContentLoaded', ()=> {
 
-    alert('press the "S" key');
+    // document.addEventListener('keydown', (event) => {
+
+    //     if (event.key === 's' || event.key === 'S') {
+
+    //         if(gameInterval) return;
+
+    //         const numElements = document.querySelectorAll('.num');
+
+    //         gameInterval = setInterval(() => {
+    //             numElements.forEach(num => {
+    //                 num.textContent = Math.floor(Math.random() * 10)
+    //             })
+    //         }, 700);
+    //     }
+    // })
+
+
+
 
     document.addEventListener('keydown', (event) => {
-        // console.log('you pressed: ', event.key);
-
         if (event.key === 's' || event.key === 'S') {
-            const numElements = document.querySelectorAll('.num');
 
-            setInterval(() => {
-                numElements.forEach(num => {
-                    num.textContent = Math.floor(Math.random() * 10)
-                })
-            }, 700);
+            Object.keys(spin).forEach(id => {
+                if (spin[id] !== null) return;
+
+                spin[id] = setInterval(() => {
+                    const element = document.getElementById(id);
+                    if (element) {
+                        element.textContent = Math.floor(Math.random() * 10)
+                    }
+                }, 700)
+            })
         }
     })
 
-    // const numElements = document.querySelectorAll('.num');
 
-    // setInterval(() => {
-    //     numElements.forEach(num => {
-    //     num.textContent = Math.floor(Math.random() * 10);
-    // });
-    // }, 700);
+
+
+
+
+    //////////////////////////////////////////////// 3rd attempt  (alomst worked)
+
+
+    // document.querySelectorAll('.child').forEach(child => {
+    //     child.addEventListener('click', (e) => {
+    //         e.target.clearInterval(gameInterval);
+    //         console.log('stopped')
+    //     })
+    // })
+
+
+})
+
+document.querySelectorAll('.child').forEach(child => {
+    child.addEventListener('click', (e) => {
+        const clickedId = e.currentTarget.id;
+
+        // clearInterval(spin[clickedId]);
+
+        // spin[clickedId] = null;
+
+        // console.log(`stopped id: ${clickedId}`)
+
+        if (spin[clickedId]) {
+            clearInterval(spin[clickedId]);
+            spin[clickedId] = null;
+            console.log(`stopped, ID: ${clickedId}`)
+        } else {
+            console.log(`did not work, ID: ${clickedId}`)
+        }
+    })
 })
